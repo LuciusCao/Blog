73e90a3896a3dfbdbc5f1f88d7f69dc35ef304bd
LuciusCao <lucius.cao@gmail.com>
2017-04-25 23:44:04 +0800

    move index out of src

---
+++ index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { Route, Redirect } from 'react-router';
+import { HashRouter } from 'react-router-dom'
+import { ApolloClient, ApolloProvider, createNetworkInterface } from 'react-apollo';
+
+import WebFont from 'webfontloader';
+
+import Test from './src/components/Test.jsx';
+import Home from './src/layouts/Home.jsx';
+import Articles from './src/layouts/Articles.jsx';
+
+const WebFontConfig = {
+  custom: {
+    families: ['Balthazar','Roboto:regular,medium'],
+    urls: ['./fonts/fonts.css']
+  }
+}
+
+WebFont.load(WebFontConfig)
+
+const networkInterface = createNetworkInterface({
+  uri: 'localhost:8000'
+});
+
+const client = new ApolloClient({
+  networkInterface: networkInterface
+});
+
+render(
+  <ApolloProvider client={client}>
+    <HashRouter>
+      <div>
+        <Route exact path='/' component={Home}/>
+        <Route path='/blogs/tech' component={Articles}/>
+        <Route path='/blogs/design' component={Test}/>
+      </div>
+    </HashRouter>
+  </ApolloProvider>,
+  document.getElementById('App')
+)
