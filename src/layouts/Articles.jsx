a1fb2153c1c6c82cc9dfa7b8a2c45d1b76522b22
LuciusCao <lucius.cao@gmail.com>
2017-04-26 00:30:03 +0800

    fixed blank margin around body and window height

--- src/layouts/Articles.jsx
+++ src/layouts/Articles.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
-const WindowHeight = window.screen.availHeight;
+const WindowHeight = window.innerHeight;
 
 const TaiChi = styled.div`
   display: flex;
